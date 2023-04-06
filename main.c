4ce8178a14d4002c12efc3a7afbcc22637c7ec3c
MEHDAOUI--JORGE ADAM <adam.mehdaoui-jorge2@heckel02.u-pem.fr>
2023-04-06 12:39:50 +0200

    display image case

--- main.c
+++ main.c
@@ -5,13 +5,12 @@
 #define NB_COL 4
 #define NB_LIG 4
 #define RES 512
-#define IMG_SIZE 512
 
 #define GAME "Taquin"
 
 typedef struct Carre{
-    int lig ;
-    int col ;
+    int lig;
+    int col;
 } Carre ;
 
 typedef struct Plateau {
@@ -28,34 +27,47 @@ void initialisationPlateau(Plateau* P) {
     }
 }
 
+int pxToInd(int px){
+    return (int)px/((int)RES/NB_COL);
+}
+
 void display_game(Plateau *P, MLV_Image *image){
     int i, j;
     int r, g, b, a;
 
-    for(i=0; i<IMG_SIZE; i++){
-        for(j=0; j<IMG_SIZE; j++){
-            MLV_get_pixel_on_image(image, i, j, &r, &g, &b, &a);
-            MLV_draw_pixel(i, j, MLV_convert_rgba_to_color(r, g, b, a));
+    for(i=0; i<NB_LIG; i++){
+        for(j=0; j<NB_COL; j++){
+
+            int startX, startY, endX, endY;
+
+            startX = i*((int)RES/NB_COL);
+            startY = j*((int)RES/NB_COL);
+            endX = startX + ((int)RES/NB_COL);
+            endY = startY + ((int)RES/NB_COL);
+
+            for(int x=startX; x<endX; x++){
+                for(int y=startY; y<endY; y++){
+                    MLV_get_pixel_on_image(image, x, y, &r, &g, &b, &a);
+                    MLV_draw_pixel(x, y, MLV_convert_rgba_to_color(r, g, b, a));
+                }
+            }
         }
     }
 }
 
+void display_grid(){
+}
+
 int main(int argc, char *argv[]){
-
-    Plateau *board;
-
-    initialisationPlateau(board); 
+    Plateau board;
+    initialisationPlateau(&board); 
 
     MLV_create_window(GAME, NULL, RES, RES); 	
+    MLV_Image* image = MLV_load_image("image.jpg"); 
 
-    MLV_Image* image = MLV_load_image("image.jpg"); 	
-
-    display_game(board, image);
-
+    display_game(&board, image);
     MLV_actualise_window();
-
     MLV_wait_seconds(10);
-
     MLV_free_window();
 
     return 0;
